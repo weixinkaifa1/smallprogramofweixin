00b3775f0c7f8497db0c1f255cb7b013b8aceeb7
zhaoning <2465832682@qq.com>
2018-09-02 22:21:40 +0800

    #zn-0902# 上传demo

---
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/DBApplication.java
@@ -0,0 +1,15 @@
+package org.linlinjava.litemall.db;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("org.linlinjava.litemall.db.dao")
+public class DBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBApplication.class, args);
+    }
+
+}
